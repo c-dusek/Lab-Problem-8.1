57b14f2780b357c2ddf9cd72a5d3ef23faed3b77
Cody Dusek <c-dusek@onu.edu>
2023-10-19 10:21:58 -0400

    Completed P8-1.

--- Lab-Problem-8.1/Source.cpp
+++ Lab-Problem-8.1/Source.cpp
@@ -2,20 +2,8 @@
 #include <string>
 using namespace std;
 
-int countVowel(string str)
-{
-	int i = str.size();
-	int vowelcount = 0;
-	for (int n = 0; n <= i; n++)
-	{
-		char indiv = str[n];
-		if (indiv == 'A' || indiv == 'a' || indiv == 'e' || indiv == 'E' || indiv == 'I' || indiv == 'i' || indiv == 'O' || indiv == 'o' || indiv == 'U' || indiv == 'u')
-		{
-			vowelcount++;
-		}
-	}
-	return vowelcount;
-}
+int countVowel(string str);
+
 int main()
 {
 	string input;
@@ -29,4 +17,18 @@ int main()
 
 	}
 
+}
+int countVowel(string str)
+{
+	int i = str.size();
+	int vowelcount = 0;
+	for (int n = 0; n <= i; n++)
+	{
+		char indiv = str[n];
+		if (indiv == 'A' || indiv == 'a' || indiv == 'e' || indiv == 'E' || indiv == 'I' || indiv == 'i' || indiv == 'O' || indiv == 'o' || indiv == 'U' || indiv == 'u')
+		{
+			vowelcount++;
+		}
+	}
+	return vowelcount;
 }
